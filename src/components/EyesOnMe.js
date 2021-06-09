6fe7404207182c58dced86235b22e2cbb9344d30
BrendanFinnegan <bf289907@gmail.com>
2021-06-09 18:54:29 -0400

    commit tree 1416f8c17b06c211979cf62e299bc8ffa4232551

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+function EyesOnMe () {
+
+    function listen(e) {
+        console.log('Good!')
+    }
+
+    function otherListen(e) {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <button onFocus={listen} onBlur={otherListen}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import Keypad from './components/Keypad';
@@ -11,3 +11,30 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+// -useState
+// -useEffect
+
+
+// destructuring Objects
+// {name, age, city} = props
+// name will equal props.name
+// age will equal props.age
+
+// destructure arrays 
+// [name, age, city] = props
+
+// spread operate when using map!!
+// {...el}
+
+// will pass all key/values down to the list item
+
+//state should be put in the lowest component that impacts all children components
+
+//Use state always takes a value then a function
+//we use array destructuring to set those in one foul swoop
+// const [value,function] = useStaet(initial value)
+
+// the goal of the function in the array is to set the value of the original value
+//do not manipulate the value of state directly
+//write a function that uses the second function that is passed to useState
